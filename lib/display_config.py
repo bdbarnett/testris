725c9ee7fbad6fa8e6011e1398f04de513ae4b92
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2023-12-18 22:52:48 -0600

    Add files via upload

--- lib/display_config.py
+++ lib/display_config.py
@@ -3,41 +3,61 @@
 # `from <this file> import display_drv``
 # or just use this file as your main.py and add your code.
 
-import mpdisplay
-from st7796 import init_sequence, rotations
-from backlight import Backlight
+from lcd_bus import I80Bus
+from st7796 import ST7796, STATE_HIGH, STATE_LOW, STATE_PWM, PORTRAIT, LANDSCAPE, BYTE_ORDER_BGR, BYTE_ORDER_RGB
 from machine import Pin
-from time import sleep
 
 # The WT32-SC01 Plus has the reset pins of the display IC and the touch IC both
 # tied to pin 4.  Controlling this pin with the display driver can lead to an
 # unresponsive touchscreen.  This case is not common.  If they aren't tied 
-# together on your board, define reset in mpdisplay.Display instead, like:
-#    mpdisplay.Display(reset=4)
+# together on your board, define reset in ST7796 instead, like:
+#    ST7796(reset=4)
 # Also do this for the display power pin if your board has one.  Most don't.
 reset=Pin(4, Pin.OUT, value=1)
 
-bus = mpdisplay.I80_bus(
-    (9, 46, 3, 8, 18, 17, 16, 15),
+bus = I80Bus(
     dc=0,
     wr=47,
     cs=6,
-    pclk=20_000_000,
-    swap_color_bytes=True,
+    data0=9,
+    data1=46,
+    data2=3,
+    data3=8,
+    data4=18,
+    data5=17,
+    data6=16,
+    data7=15,
+    max_transfer_bytes=1048576,
+    freq=20000000,
+    dc_idle_level=0,
+    dc_cmd_level=0,
+    dc_dummy_level=0,
+    dc_data_level=1,
+    cmd_bits=8,
+    param_bits=8,
+    cs_active_high=False,
     reverse_color_bits=False,
+    swap_color_bytes=True,
+    pclk_active_neg=False,
+    pclk_idle_low=False,
 )
 
-display_drv = mpdisplay.Display(
+display_drv = ST7796(
     bus,
-    width=320,
-    height=480,
+    display_width=320,
+    display_height=480,
+    reset_pin=None,
+    reset_state=STATE_HIGH,
+    power_pin=None,
+    power_on_state=STATE_HIGH,
+    backlight_pin=45,
+    backlight_on_state=STATE_HIGH,
+    offset_x=0,
+    offset_y=0,
+    color_byte_order=BYTE_ORDER_BGR,
+    orientation=PORTRAIT,
+    color_space=None,
     bpp=16,
-    reset=-1,
-    rotation=0,
-    bgr=True,
-    invert_color=True,
-    init_sequence=init_sequence,
-    rotations=rotations,
 )
 
-backlight=Backlight(Pin(45, Pin.OUT), on_high=True)
+display_drv.init()
